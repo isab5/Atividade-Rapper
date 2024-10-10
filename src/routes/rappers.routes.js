3bce671f737d9124342f55b1786192b69fce90c3
Isabella Borin <isabellaborin.tds1.senai@gmail.com>
2024-10-10 09:56:37 -0300

    feat: create method post

--- src/routes/rappers.routes.js
+++ src/routes/rappers.routes.js
@@ -8,19 +8,53 @@ let rappers = [
     nome: "Shawn Corey Carter",
     idade: 54,
     descricaoFisica: "Alto, negro, dread/crespo",
-    suspeito: true,
+    envolvimento: true,
 },
 {
     id: Math.floor(Math.random() * 1000),
     nome: "Marshall Bruce Mathers III",
     idade: 51,
     descricaoFisica: "Branco, barba, cabelo preto, estatura média",
-    suspeito: false,
+    envolvimento: false,
 },
 ];
 
 rappersRoutes.get("/", (req, res) => {
 return res.status(200).json(rappers);
 });
+
+rappersRoutes.post("/", (req, res) => {
+    const { nome, idade, descricaoFisica, envolvimento } = req.body;
+    
+    console.log(Number.isInteger(idade));
+
+    if (!nome) {
+    return res.status(400).json({
+        message: "O campo nome é obrigatório!",
+    });
+    }
+
+    if (envolvimento != 'sim' && envolvimento != 'não') {
+    return res.status(400).send({
+        message: "Digite 'sim' ou 'não'",
+    });
+    }
+
+    const novoSuspeito = {
+    id: Math.floor(Math.random() * 1000),
+    nome,
+    idade,
+    descricaoFisica,
+    envolvimento,
+    };
+
+    rappers.push(novoSuspeito);
+
+    return res.status(201).json({
+    message: "Suspeito cadastrado com sucesso!",
+    novoSuspeito,
+    });
+});
+
 
 export default rappersRoutes;
